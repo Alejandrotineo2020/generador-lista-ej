2751d1d9bbcbd48dfb40ef543a18b9a34963b106
Alejandro M. Tineo Moreta <115231713+Alejandrotineo2020@users.noreply.github.com>
2026-03-16 20:23:03 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace numero8
+{
+    public partial class MainPage : ContentPage
+    {
+       public List<int> MisNumeros { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+         
+
+
+        }
+
+        private void OnGenerarClicked (object sender, EventArgs e)
+        {
+            if (int.TryParse(LimiteEntry.Text, out int numeroUsuario))
+            {
+                GenerarNumeros(numeroUsuario);
+            }
+        }
+
+
+
+        private void GenerarNumeros (int limite)
+        {
+            List<int> listaNumeros = new List<int>();
+
+            for (int i = 1; i <= limite; i++)
+            {
+                listaNumeros.Add(i);
+            }
+
+
+            NumerosListView.ItemsSource = listaNumeros;
+        }
+
+
+
+    }
+}
